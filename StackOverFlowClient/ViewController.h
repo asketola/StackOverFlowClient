8f788fdf90158e8cc51d6c72e1e975acdcd21171
Anne <star7anne@yahoo.com>
2015-02-17 13:25:42 -0800

    Initial Commit

---
+++ StackOverFlowClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverFlowClient
+//
+//  Created by Annemarie Ketola on 2/17/15.
+//  Copyright (c) 2015 Up Early, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StackOverFlowClient/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StackOverFlowClient
+//
+//  Created by Annemarie Ketola on 2/17/15.
+//  Copyright (c) 2015 Up Early, LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
